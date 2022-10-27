4b39207ba69ace4c64c04c42b94ab822dd090303
Albert Toshiro Heru <54266995+alberttoshiro@users.noreply.github.com>
2022-10-27 17:08:13 +0700

    Add files via upload

--- src/main/java/com/albert/processdata/ProcessData.java
+++ src/main/java/com/albert/processdata/ProcessData.java
@@ -5,87 +5,68 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import com.albert.dao.BaseDAO;
+import com.albert.model.BaseTable;
 
-import com.albert.postgresql.JDBCPostgreSQLConnect;
+public abstract class ProcessData<T extends BaseDAO<U>, U extends BaseTable> implements Runnable {
+  private String folderName;
+  private String filePath;
+  private T DAO;
 
-public class ProcessData implements Runnable {
-	private String folderName;
-	private String filePath;
-	private String query;
-	private String[] dataType;
+  public ProcessData(String folderName, String filePath, T DAO) {
+    super();
+    this.folderName = folderName;
+    this.filePath = filePath;
+    this.DAO = DAO;
+  }
 
-	public ProcessData(String folderName, String filePath, String query, String[] dataType) {
-		super();
-		this.folderName = folderName;
-		this.filePath = filePath;
-		this.query = query;
-		this.dataType = dataType;
-	}
+  public abstract U convertToU(String[] param);
 
-	public String getFolderName() {
-		return folderName;
-	}
+  public String getFilePath() {
+    return filePath;
+  }
 
-	public void setFolderName(String folderName) {
-		this.folderName = folderName;
-	}
+  public String getFolderName() {
+    return folderName;
+  }
 
-	public String getFilePath() {
-		return filePath;
-	}
+  public void moveFile() {
+    try {
+      Files.move(Paths.get(filePath),
+          Paths.get(filePath.replace(folderName, folderName + "-done")));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-	public void setFilePath(String filePath) {
-		this.filePath = filePath;
-	}
+  public boolean processFile() {
+    try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+      String line = "";
+      while ((line = br.readLine()) != null) {
+        String[] param = line.split(";");
+        U u = convertToU(param);
+        DAO.insert(u);
+      }
+      return true;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
 
-	public String getQuery() {
-		return query;
-	}
+  @Override
+  public void run() {
+    if (processFile()) {
+      moveFile();
+    }
+  }
 
-	public void setQuery(String query) {
-		this.query = query;
-	}
+  public void setFilePath(String filePath) {
+    this.filePath = filePath;
+  }
 
-	public String[] getDataType() {
-		return dataType;
-	}
-
-	public void setDataType(String[] dataType) {
-		this.dataType = dataType;
-	}
-
-	public boolean processFile() {
-		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-			String line = "";
-			JDBCPostgreSQLConnect sqlCon = new JDBCPostgreSQLConnect();
-			while((line = br.readLine()) != null) {
-				Object[] param = line.split(";");
-//				for(int i = 0 ; i < param.length ; i++) {
-//					System.out.print(param[i].toString() + ", " );
-//				}
-//				System.out.println();
-				sqlCon.executeUpdate(query, param, dataType);
-			}
-			return true;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-
-	public void moveFile() {
-		try {
-			Files.move(Paths.get(filePath), Paths.get(filePath.replace(folderName, folderName + "-done")));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public void run() {
-		if(processFile()) {
-			moveFile();
-		}
-	}
+  public void setFolderName(String folderName) {
+    this.folderName = folderName;
+  }
 
 }
